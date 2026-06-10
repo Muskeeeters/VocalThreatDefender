1cff38012d66450d673140a94ec12ca7f1762067
itsEkramah <ramlahmunir786@gmail.com>
2026-06-10 10:20:03 +0500

    .

--- voiceshield/wsgi.py
+++ voiceshield/wsgi.py
@@ -1,13 +1,4 @@
-"""
-voiceshield/wsgi.py
-===================
-WSGI config for the VoiceShield AI project.
-Exposes the WSGI callable as a module-level variable named ``application``.
-"""
-
 import os
 from django.core.wsgi import get_wsgi_application
-
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "voiceshield.settings")
-
 application = get_wsgi_application()
